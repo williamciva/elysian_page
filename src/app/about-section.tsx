3a220fa2f966c4a9071a6dcf7f7d7051b015fa36
DolAndi <DolAndi@users.noreply.github.com>
2024-08-06 12:09:18 +0000

    deploy: ElysianBR/aphrodite@af6aaa86131c61519d4cf0dc561992b36c0cf80e

--- src/app/about-section.tsx
+++ src/app/about-section.tsx
@@ -17,11 +17,16 @@ const AboutSection: FC<AboutProps> = (props) => {
                 style={{ backgroundImage: 'url(/path/to/your/background-image.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}
             >
                 <Container>
-                    <Typography variant={props.isMobile ? 'h5' : 'h3'} textAlign="center" gutterBottom>
+                    <Typography variant={props.isMobile ? 'h5' : 'h3'} textAlign="center" fontWeight="bold" gutterBottom>
                         Transforme seu negócio com a blockchain
                     </Typography>
                     <Typography variant="body1" textAlign="center">
-                        Entre na nova era digital, e revolucione o mercado com soluções blockchain inovadoras.
+                    Na Elysian, acreditamos que a tecnologia blockchain pode
+                    revolucionar a maneira como as empresas operam, trazendo
+                    transparência, segurança e eficiência sem precedentes. Somos uma
+                    startup inovadora focada em desenvolver contratos inteligentes,
+                    integrar sistemas e oferecer suporte de qualidade para garantir que
+                    sua transição para a nova era digital seja suave e bem-sucedida.
                     </Typography>
                 </Container>
             </Box>
@@ -33,33 +38,45 @@ const AboutSection: FC<AboutProps> = (props) => {
                         <Grid item xs={12} sm={6} md={4}>
                             <Box display="flex" flexDirection="column" alignItems="center" mb={2}>
                                 <Image src="/security.png" alt="Segurança Máxima" width={50} height={50} />
-                                <Typography variant="h6" textAlign="center" gutterBottom>
+                                <Typography variant="h6" textAlign="center" fontWeight="bold" gutterBottom>
                                     Segurança Máxima
                                 </Typography>
-                                <Typography variant="body2" textAlign="center">
-                                    Garanta a segurança dos dados com criptografia avançada e tecnologia de ponta.
+                                <Typography variant="body2" textAlign="justify">
+                                Com a Elysian, seus dados estão protegidos por criptografia
+                                avançada e tecnologia de ponta, garantindo a máxima segurança
+                                nas transações.
                                 </Typography>
                             </Box>
                         </Grid>
                         <Grid item xs={12} sm={6} md={4}>
                             <Box display="flex" flexDirection="column" alignItems="center" mb={2}>
                                 <Image src="/clock.png" alt="Eficiência e Velocidade" width={50} height={50} />
-                                <Typography variant="h6" textAlign="center" gutterBottom>
+                                <Typography variant="h6" textAlign="center" fontWeight="bold" gutterBottom>
                                     Eficiência e Velocidade
                                 </Typography>
-                                <Typography variant="body2" textAlign="center">
-                                    Processe transações rapidamente, sem intermediários, reduzindo custos e tempo.
+                                <Typography variant="body2" textAlign="justify">
+                                Nossa plataforma elimina intermediários, permitindo processar
+                                transações rapidamente e com menor custo. Oferecemos
+                                integração fácil com sistemas ERP existentes, proporcionando
+                                uma visão unificada e precisa de suas operações. Com a
+                                tecnologia blockchain, reduzimos erros e fraudes, tornando
+                                suas movimentações de estoque mais eficientes.
                                 </Typography>
                             </Box>
                         </Grid>
                         <Grid item xs={12} sm={6} md={4}>
                             <Box display="flex" flexDirection="column" alignItems="center" mb={2}>
                                 <Image src="/idea.png" alt="Inovação e Crescimento" width={50} height={50} />
-                                <Typography variant="h6" textAlign="center" gutterBottom>
+                                <Typography variant="h6" textAlign="center" fontWeight="bold" gutterBottom>
                                     Inovação e Crescimento
                                 </Typography>
-                                <Typography variant="body2" textAlign="center">
-                                    Explore novos modelos de negócios e abra portas para o crescimento exponencial.
+                                <Typography variant="body2" textAlign="justify">
+                                Estamos constantemente investindo em pesquisa e desenvolvimento
+                                para trazer as mais recentes inovações ao seu negócio. Explore
+                                novos modelos de negócios e abra portas para o crescimento
+                                exponencial com nossas soluções personalizadas. Se você está
+                                pronto para dar o próximo passo na revolução digital, a Elysian
+                                está aqui para ajudá-lo a alcançar novos patamares.
                                 </Typography>
                             </Box>
                         </Grid>
