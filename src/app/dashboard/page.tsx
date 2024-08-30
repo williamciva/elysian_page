515ad75b24faf6cfa32372cf1268f173ed99b6c9
DolAndi <DolAndi@users.noreply.github.com>
2024-08-30 00:05:05 +0000

    deploy: ElysianBR/aphrodite@5a6f02a1c5357690c30d0e737f1cff838f507f92

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import React from 'react';
+import DashboardTemplate from '../../components/dashboard/Dashboard'; 
+import '../../app/dashboard/dashboard.css';
 
 const Dashboard = () => {
     return (
-        <div className="p-6">
-            <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
-            <p>Hello World!</p>
-        </div>
+        <DashboardTemplate>
+            <div className="p-6">
+            </div>
+        </DashboardTemplate>
     );
 };
 
---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import {
+  PaletteMode,
+  createTheme,
+  ThemeProvider,
+  alpha,
+} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import getDashboardTheme from './theme/getDashboardTheme';
+import AppNavbar from './components/AppNavbar';
+import Header from './components/Header';
+import MainGrid from './components/MainGrid';
+import SideMenu from './components/SideMenu';
+import TemplateFrame from './TemplateFrame';
+
+interface DashboardTemplateProps {
+  children: React.ReactNode; // Adiciona children como prop
+}
+
+export default function DashboardTemplate({ children }: DashboardTemplateProps) {
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const dashboardTheme = createTheme(getDashboardTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  React.useEffect(() => {
+    const savedMode = localStorage.getItem('themeMode') as PaletteMode | null;
+    if (savedMode) {
+      setMode(savedMode);
+    } else {
+      const systemPrefersDark = window.matchMedia(
+        '(prefers-color-scheme: dark)',
+      ).matches;
+      setMode(systemPrefersDark ? 'dark' : 'light');
+    }
+  }, []);
+
+  const toggleColorMode = () => {
+    const newMode = mode === 'dark' ? 'light' : 'dark';
+    setMode(newMode);
+    localStorage.setItem('themeMode', newMode);
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
+  return (
+    <TemplateFrame
+      toggleCustomTheme={toggleCustomTheme}
+      showCustomTheme={showCustomTheme}
+      mode={mode}
+      toggleColorMode={toggleColorMode}
+    >
+      <ThemeProvider theme={showCustomTheme ? dashboardTheme : defaultTheme}>
+        <CssBaseline enableColorScheme />
+        <Box sx={{ display: 'flex' }}>
+          <SideMenu />
+          <AppNavbar />
+          <Box
+            component="main"
+            sx={(theme) => ({
+              flexGrow: 1,
+              backgroundColor: alpha(theme.palette.background.default, 1),
+              overflow: 'auto',
+            })}
+          >
+            <Stack
+              spacing={2}
+              sx={{
+                alignItems: 'center',
+                mx: 3,
+                pb: 10,
+                mt: { xs: 8, md: 0 },
+              }}
+            >
+              <Header />
+              <MainGrid />
+              {children} {/* Renderiza o conteúdo passado como children */}
+            </Stack>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </TemplateFrame>
+  );
+}
